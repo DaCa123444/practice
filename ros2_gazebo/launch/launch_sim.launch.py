782808bf1552f79369dda4cd6f2bc541a21a4c7a
DaCa123444 <145843942+DaCa123444@users.noreply.github.com>
2024-02-19 22:03:14 +0900

    Add files via upload

---
+++ ros2_gazebo/launch/launch_sim.launch.py
@@ -0,0 +1,36 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+    package_name='ros2_gazebo' 
+
+	#rob state pub
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # gzserver
+    gzserver = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')]),
+             )
+
+    # Launch
+    return LaunchDescription([
+        rsp,
+        gzserver
+    ])
+
